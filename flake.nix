408e55ea7aae223f63d69ed293ee492acce243a3
poz <poz@poz.pet>
2026-01-02 19:22:29 +0100

    refactor flake and get rid of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,61 +1,68 @@
-
 {
   description = "Zoomer application for wayland inspired by tsoding's boomer";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, systems, crane, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      checks = forEachSystem (system: {
+        inherit (self.packages.${system}) woomer;
+      });
 
+      packages = forEachSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
         craneLib = crane.mkLib pkgs;
 
         # Common arguments can be set here to avoid repeating them later
         # Note: changes here will rebuild all dependency crates
-        commonArgs = rec {
-          src = let
-              shaderFilter = path: _type: builtins.match ".*fs$" path != null;
-              shaderOrCargo = path: type:
-                (shaderFilter path type) || (craneLib.filterCargoSources path type);
-            in
-            pkgs.lib.cleanSourceWith {
-              src = craneLib.path ./.;
-              filter = shaderOrCargo;
-            };
-          strictDeps = true;
-          nativeBuildInputs = (with pkgs; [
-            cmake
-            pkg-config
-            clang
-            wayland
-          ]);
-          buildInputs = (with pkgs; [
-            wayland
-            glfw
-            libgbm
-          ]) ++ (
-            with pkgs.xorg; [
-            libX11.dev
-            libXrandr.dev
-            libXinerama.dev
-            libXcursor.dev
-            libXi.dev
-          ]);
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          LIBCLANG_PATH = pkgs.libclang.lib + "/lib/";
-        };
+        commonArgs =
+          let
+            buildInputs = (with pkgs; [
+              wayland
+              glfw
+              libgbm
+            ]) ++ (
+              with pkgs.xorg; [
+              libX11.dev
+              libXrandr.dev
+              libXinerama.dev
+              libXcursor.dev
+              libXi.dev
+            ]);
 
-        woomer = craneLib.buildPackage (commonArgs // {
+            craneLib = crane.mkLib pkgs;
+          in {
+            src = let
+                shaderFilter = path: _type: builtins.match ".*fs$" path != null;
+                shaderOrCargo = path: type:
+                  (shaderFilter path type) || (craneLib.filterCargoSources path type);
+              in
+              pkgs.lib.cleanSourceWith {
+                src = craneLib.path ./.;
+                filter = shaderOrCargo;
+              };
+            strictDeps = true;
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkg-config
+              clang
+              wayland
+            ];
+            inherit buildInputs;
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            LIBCLANG_PATH = pkgs.libclang.lib + "/lib/";
+          };
+      in {
+        woomer = self.packages.${system}.default;
+        default = craneLib.buildPackage (commonArgs // {
           cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
           postFixup = ''
@@ -66,32 +73,38 @@
               --add-rpath ${pkgs.lib.makeLibraryPath [ pkgs.wayland ]}
           '';
 
-          meta = with nixpkgs.lib; {
+          meta = {
             description = "Zoomer application for Wayland inspired by tsoding's boomer";
-            license = licenses.mit;
+            license = pkgs.lib.licenses.mit;
             mainProgram = "woomer";
           };
         });
-      in
-      {
-        checks = {
-          inherit woomer;
-        };
+      });
 
-        packages.default = woomer;
+      apps = forEachSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        type = "app";
+        program = pkgs.lib.getExe self.packages.${system}.default;
+      });
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = woomer;
-        };
-
-        devShells.default = craneLib.devShell {
-          inherit (commonArgs) nativeBuildInputs buildInputs LIBCLANG_PATH LD_LIBRARY_PATH;
-          # Inherit inputs from checks.
+      devShells = forEachSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in {
+        default = craneLib.devShell {
           checks = self.checks.${system};
           packages = with pkgs; [
             rust-analyzer
           ];
-
+          env = {
+            inherit (self.packages.${system}.default)
+              LIBCLANG_PATH LD_LIBRARY_PATH;
+          };
+          inputsFrom = [
+            self.packages.${system}.default
+          ];
         };
       });
+    };
 }
